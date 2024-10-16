ee6cbd104577e078db58fb23b1a6220c039e2ce7
jpkh0120 <jpkh0120@JASON-POH>
2024-10-16 09:59:15 +0800

    First Commit

---
+++ Programming/src/test/Name.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java\n");
+		System.out.println("Learning Java Now"); 
+		System.out.println("Programming is fun\n");
+		
+		for (int i=1; i<=5;i++) {
+			System.out.println("I love Java");
+		
+		}
+		
+		System.out.println("\n      J  ");
+		System.out.println("J aaa v vaaa"); 
+		System.out.println("J J aa vv a a");
+		System.out.println("J aaa v aaa");
+
+	}
+
+}
---
+++ Programming/src/test/Test1.java
@@ -0,0 +1,12 @@
+package test;
+public class Test1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World: ");
+		System.out.print("Hello World: ");
+		
+		
+		
+	}
+
+}
