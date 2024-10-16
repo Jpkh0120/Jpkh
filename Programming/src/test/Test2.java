c4b16b13de0d1764a03e2d27ea175ef04c421ba9
jpkh0120 <jpkh0120@JASON-POH>
2024-10-16 11:07:39 +0800

    Second

--- Programming/src/test/Test2.java
+++ Programming/src/test/Test2.java
@@ -2,7 +2,31 @@ package test;
 
 public class Test2 {
 	public static void main (String [] args) {
-		System.out.print("Hello");
+		System.out.println("Poh Kok Hao");
+		
+		System.out.println("\nPoh Kok Hao");
+		System.out.println("229099");
+		System.out.println("No 11, Jalan Villa 1, Taman Anggerik Villa 43500 Semenyih Selangor");
 
+		System.out.println("\nWelcome to Java\n"); 
+		System.out.println("Learning Java Now");
+		System.out.println("Programming is Fun\n");
+		
+		for (int i=1; i<=5 ;i++) {
+		System.out.println("I love Java");
+		}
+		
+		System.out.println("\n     J");
+		System.out.println("J aaa v vaaa");
+		System.out.println("J J aa vv a a ");
+		System.out.println("J aaaa v aaa");
+		
+		System.out.println("\n************************************************");
+		System.out.println("*                                              *");
+		System.out.println("* 	Making Money Bank ATM Service          *");                            
+		System.out.println("*                                              *");
+		System.out.println("************************************************");
+		System.out.println("\n\nWelcome!");
+		
 	}
 }
