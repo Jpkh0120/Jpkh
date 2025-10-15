7bd20e7c049ddfff4c8b1b956aba3bd0c84a2af8
Jpkh0120 <haokok20@gmail.com>
2025-10-15 10:34:14 +0800

    first

--- Rectangle.java
+++ Rectangle.java
@@ -1,18 +1,23 @@
-class Rectangle{
-   double width; double height;
-   Rectangle(){ //constuctor with no argument
-      width=1;
-      height=1;
+class Rectangle {
+   double width;
+   double height;
+
+   Rectangle() { // constuctor with no argument
+      width = 2;
+      height = 1;
    }
-   Rectangle (double newWidth, double newHeight){ //constuctor with argument
-      width=newWidth; 
-      height=newHeight;
+
+   Rectangle(double newWidth, double newHeight) { // constuctor with argument
+      width = newWidth;
+      height = newHeight;
    }
-   double getArea(){ //method to calculate area
-      return width*height;         
+
+   double getArea() { // method to calculate area
+      return width * height;
    }
-   double getPerimeter(){ //method to calculate perimeter
-      return 2*(width+height);         
+
+   double getPerimeter() { // method to calculate perimeter
+      return 2 * (width + height);
    }
-   
+
 }
