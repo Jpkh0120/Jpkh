17e189d89bd4af06e37025589ee2b4b6ac635be6
Jpkh0120 <haokok20@gmail.com>
2025-10-15 14:07:21 +0800

    commit

--- TestRectangle.java
+++ TestRectangle.java
@@ -1,21 +1,19 @@
-public class TestRectangle{
-   public static void main (String [] args){
-      //Test Rectangle 1
-      Rectangle rectangle1=new Rectangle(4,40);
-      System.out.println("Rectangle 2s: ");
-      System.out.printf("Width: %.1fcm\n",rectangle1.width);
-      System.out.printf("Height: %.1fcm\n",rectangle1.height);
-      System.out.printf("Area: %.1fcm2\n",rectangle1.getArea());
-      System.out.printf("Perimter: %.1fcm\n",rectangle1.getPerimeter());
+public class TestRectangle {
+   public static void main(String[] args) {
+      // Test Rectangle 1
+      Rectangle rectangle1 = new Rectangle(4, 40);
+      System.out.println("Rectangle s: ");
+      System.out.printf("Width: %.1fcm\n", rectangle1.width);
+      System.out.printf("Height: %.1fcm\n", rectangle1.height);
+      System.out.printf("Area: %.1fcm2\n", rectangle1.getArea());
+      System.out.printf("Perimter: %.1fcm\n", rectangle1.getPerimeter());
       System.out.println("\nRectangle 2: ");
-      
-      //Test Rectangle 2
-      Rectangle rectangle2=new Rectangle(3.,35.9);
-      System.out.printf("Width: %.1fcm\n",rectangle2.width);
-      System.out.printf("Height: %.1fcm\n",rectangle2.height);
-      System.out.printf("Area: %.1fcm2\n",rectangle2.getArea());
-      System.out.printf("Perimeter: %.1fcm\n ",rectangle2.getPerimeter());
+
+      // Test Rectangle 2
+      Rectangle rectangle2 = new Rectangle(3., 35.9);
+      System.out.printf("Width: %.1fcm\n", rectangle2.width);
+      System.out.printf("Height: %.1fcm\n", rectangle2.height);
+      System.out.printf("Area: %.1fcm2\n", rectangle2.getArea());
+      System.out.printf("Perimeter: %.1fcm\n ", rectangle2.getPerimeter());
    }
 }
-
-
